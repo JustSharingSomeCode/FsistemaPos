9b53e61c95a9bf0cfe50efa10b1e0290dcb24504
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-07-28 17:17:00 -0500

    show components on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FsistemaPos';
+
+  divClients:boolean = false;
+  divProducts:boolean = false;
+  divSales:boolean = false;
+  divInvoices:boolean = true;
+
+  hideAll()
+  {
+    this.divClients = false;
+    this.divProducts = false;
+    this.divSales = false;
+    this.divInvoices = false;
+  }
+
+  showClients()
+  {
+    this.hideAll();
+    this.divClients = true;
+  }
+
+  showProducts()
+  {
+    this.hideAll();
+    this.divProducts = true;
+  }
+
+  showSales()
+  {
+    this.hideAll();
+    this.divSales = true;
+  }
+
+  showInvoices()
+  {
+    this.hideAll();
+    this.divInvoices = true;
+  }
 }
+
