6730efae7d9cdbd3e84ae8426228001d6baf2beb
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-07-29 17:46:14 -0500

    add product form

--- src/app/components/products-component/products-component.component.ts
+++ src/app/components/products-component/products-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IProduct } from 'src/app/interfaces/iproduct';
 
 @Component({
   selector: 'app-products-component',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponentComponent implements OnInit {
 
-  constructor() { }
+  action = "Add"
+  editMode = false;
+  editId = "";
+
+  productList: IProduct[] = [
+    {
+      productId: 1,
+      pName: "Lapiz norma",
+      pDescription: "Lapiz negro b2 amarillo",
+      stock: 15,
+      img: "https://d2d21jw8en5l3a.cloudfront.net/vendty2_db_39174_mari2020/imagenes_productos/327_lpiz_de_grafito_hb_n2_negro_norma_imagen.jpeg",
+      price: 2500
+    },
+    {
+      productId: 2,
+      pName: "Resma blanca",
+      pDescription: "",
+      stock: 20,
+      img: "https://http2.mlstatic.com/D_NQ_NP_811560-MCO41309377802_042020-O.jpg",
+      price: 12000
+    }
+  ];
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      pName: ["",[Validators.required, Validators.maxLength(150)]],
+      pDescription: ["", [Validators.required, Validators.maxLength(250)]],
+      stock: ["", Validators.required],
+      img: ["", [Validators.required, Validators.maxLength(350)]],
+      price: ["", Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  getProducts()
+  {
+
+  }
+
+  saveProduct()
+  {
+
+  }
+
+  editProduct()
+  {
+
+  }
+
+  deleteProduct()
+  {
+
+  }
+
+  cancelEdit()
+  {
+
+  }
 }
