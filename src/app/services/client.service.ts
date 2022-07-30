f41ffe4a4dbeefaeeff8d3257e040a127500190b
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-07-29 20:53:41 -0500

    product service and environment variable

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IClient } from '../interfaces/iclient';
 
 
@@ -9,7 +10,7 @@ import { IClient } from '../interfaces/iclient';
 })
 export class ClientService {
 
-  private myAppUrl = "https://localhost:7044/";
+  private myAppUrl = environment.appUrl;
   private myApiUrl = "api/Clients/";
 
   constructor(private http: HttpClient) { }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IProduct } from '../interfaces/iproduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private myAppUrl = environment.appUrl;
+  private myApiUrl = "api/Products/";
+
+  constructor(private http: HttpClient) { }
+
+  getProductList(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.myAppUrl + this.myApiUrl);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(this.myAppUrl + this.myApiUrl + id)
+  }
+
+  saveProduct(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(this.myAppUrl + this.myApiUrl, product);
+  }
+
+  updateProduct(id: number, product: IProduct): Observable<any> {
+    return this.http.put(this.myAppUrl + this.myApiUrl + id, product);
+  }
+}
